a84e3e50c411a2256918351cfae6e0c5d4a444b6
Sarthak Mohanty <sarthak@sarthakmohanty.me>
2024-04-29 21:30:15 -0400

    fix: jasper's pr

--- components/head.js
+++ components/head.js
@@ -3,12 +3,7 @@ import Head from 'next/head'
 export default function HeadObject({children}) {
     const title = "Ham Club";
     const description = "Earn your Callsign, Get a radio.";
-    // const searchBarColor = "#ffffff"; // This is your Safari 15 Search Bar Color in Light Mode
-    //const darkSearchBarColor = "#000000"; // This is your Safari 15 Search Bar Color in Dark Mode (optional)
-    // const keywords = "put some fun keywords here";
-    // const author = "Sarthak Mohanty";
-    const url = "https://ham.hackclub.com"; // This is your og:url or domain (optional but recommended)
-    //const image = "/ogimage.png"; // This is your OpenGraph image
+    const url = "https://ham.hackclub.com";
     return (
         <Head>
             <meta charSet="utf-8" />
@@ -16,19 +11,11 @@ export default function HeadObject({children}) {
             <meta name="viewport" content="width=device-width,initial-scale=1" />
             <title>{title}</title>
             <meta name="description" content={description} />
-            <meta name="keywords" content={keywords} />
-            <meta name="author" content={author} />
-            <meta name="theme-color" content={searchBarColor} media="(prefers-color-scheme: light)" />
             <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😋</text></svg>" />
-            {/* <meta name="theme-color" content={darkSearchBarColor} media="(prefers-color-scheme: dark)" /> */}
             {url ? <meta property="og:url" content={url} /> : ''}
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            {/* <meta property="og:image" content={image} /> */}
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content={twitter} />
-            <meta name="twitter:creator" content={twitter} />
             {/* Add analytics here */}
             {children}
         </Head>
